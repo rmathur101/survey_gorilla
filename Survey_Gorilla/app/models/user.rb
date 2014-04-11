4d69e5766c0dee242b809299f2d25b1960fd7082
Apprentice <apprentice@dbc-chicago-024.local>
2014-04-11 14:17:59 -0500

    Added associations for models and added Choice and Round models

--- Survey_Gorilla/app/models/user.rb
+++ Survey_Gorilla/app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   validates :name, :length => { :minimum => 3, :message => "must be at least 3 characters, fool!" }
   # validates :entered_password, :length => { :minimum => 6 }
   validates :email, :uniqueness => true, :format => /.+@.+\..+/ # imperfect, but okay
+
   has_many :surveys , foreign_key: "creator_id"
   has_many :rounds, foreign_key: "taker_id"
 
