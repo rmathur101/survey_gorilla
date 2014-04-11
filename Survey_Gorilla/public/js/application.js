644ee46b0c5a8d64cf24dd2cb24aa19c099cbfb4
Rohan Mathur <rmathur101@gmail.com>
2014-04-11 11:30:50 -0500

    Added after-login view

--- Survey_Gorilla/public/js/application.js
+++ Survey_Gorilla/public/js/application.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  
 });
+
+
+
+
+
+// THIS CAN BE USED IN YOUR ERB FILE AND CAN YET ANOTHER WAY TO TRANSFER INFORMATION FROM YOUR SERVER TO THE DOM 
+// <script type = "text/javascript">
+//   var person = "nate";
+//   var currentUserName = "<%= 2+2 %>"; 
+// </script>
