4ad0e4bb835c272ab9325f8a7355926398c06d0b
Apprentice <apprentice@dbc-chicago-024.local>
2014-04-11 15:29:06 -0500

    Added some helpers for determining of a survey has been completed and a logged in helper

--- Survey_Gorilla/app/models/round.rb
+++ Survey_Gorilla/app/models/round.rb
@@ -2,4 +2,19 @@ class Round < ActiveRecord::Base
   belongs_to :taker, class_name: "User"
   belongs_to :survey
   has_many :responses
+
+  def self.round_complete?(round_id)
+    round = Round.find(round_id)
+    question_count = round.survey.questions.count
+    taker_responses = round.taker.responses.where(round_id: round_id).count
+    return question_count == taker_responses
+  end
+
+  def self.set_round_complete(round_id)
+    if round_complete?(round_id)
+      round = Round.find(round_id)
+      round.taken = round_complete?(round_id)
+      round.save
+    end
+  end
 end
--- Survey_Gorilla/app/models/user.rb
+++ Survey_Gorilla/app/models/user.rb
@@ -10,5 +10,6 @@ class User < ActiveRecord::Base
 
   has_many :surveys , foreign_key: "creator_id"
   has_many :rounds, foreign_key: "taker_id"
+  has_many :responses, through: :rounds
 
 end
