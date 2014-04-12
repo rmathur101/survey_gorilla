117359f5685cdfb4ba24e196eb82986e175fd8ad
Apprentice <apprentice@dbc-chicago-024.local>
2014-04-12 16:09:19 -0500

    tried to fix merge conflict

--- Survey_Gorilla/public/js/application.js
+++ Survey_Gorilla/public/js/application.js
@@ -1,3 +1,8 @@
+var optionNode = "<li><input class='option' type='text' name='survey[questions][][options][]' placeholder='an option'> <button tabindex = '-1' class='remove-button'>✖</button></li>";
+
+var questionNode = "<li><input class='question' type='text' name='survey[questions][][question]' placeholder='What do you want to ask?'> <button tabindex = '-1' class='remove-button'>✖</button> <button tabindex = '-1' class='button add-option'>add option</button><ul>"+optionNode+"</ul></li>";
+
+
 $(document).ready(function() {
     $("#check_email").hide();
     $("#check_password").hide();
@@ -47,56 +52,88 @@ $(document).ready(function() {
   });
 
 
+
+
+  $('#survey-builder > ol').append(questionNode);
+  console.log("ANYONE HERE");
+  $('#survey-builder').on('click', '.add-question', function(event){
+    event.preventDefault();
+    console.log("HOW ABOUT HERE?");
+    $($(this).siblings('ol')).append(questionNode);
+  });
+
+  $('#survey-builder').on('click', '.add-option', function(event){
+    event.preventDefault();
+    $($(this).siblings('ul')).append(optionNode);
+  });
+
+  $('#survey-builder').on('click', '.remove-button', function(event){
+    event.preventDefault();
+    $(this).parent().remove();
+  });
+});
+
+
 //so we want to do the following actions using objects instead. how do we accomplish this? we can create a question object. what attributes will a question object have?
 
 
-  function Question(question_counter){
-    this.question_options = [];
-    this.question_options_counter = 1;
-    this.question_html = "<div class='question_content'>Question " + question_counter +": <input name='survey_question_"+question_counter+"' type='text'><br>Option "+ this.question_options_counter+" <input name='survey_quest_"+question_counter+"_resp"+question_counter+"' type='text'><br><br><button id='option_add_"+this.question_options_counter+"'>Add an option</button><button id='option_remove_"+this.question_options_counter+"'>Remove an option</button><br><br></div>";
-  }; //what we pass into the questoin will determine how we distinguish questions (maybe we should be passing in an index)
+  // function Question(question_counter){
+  //   this.question_options = [];
+  //   this.question_options_counter = 1;
+  //   this.question_html = "<div class='question_content'>Question " + question_counter +": <input name='survey_question_"+question_counter+"' type='text'><br>Option "+ this.question_options_counter+" <input name='survey_quest_"+question_counter+"_resp"+question_counter+"' type='text'><br><br><button id='option_add_"+this.question_options_counter+"'>Add an option</button><button id='option_remove_"+this.question_options_counter+"'>Remove an option</button><br><br></div>";
+  // }; //what we pass into the questoin will determine how we distinguish questions (maybe we should be passing in an index)
 
-  function Option(position) {
-    this.position = position;
-  };
-  //what is the position? is this indicating what option it is? maybe this should not be determined by how we pass something into the function. there must be a more viable way to do it.
+  // function Option(position) {
+  //   this.option_position = position;
+  //   this.option_html = "Option "+ this.question_options_counter+" <input name='survey_quest_"+question_counter+"_resp"+question_counter+"' type='text'><br><br><button id='option_add_"+this.question_options_counter+"'>Add an option</button><button id='option_remove_"+this.question_options_counter+"'>Remove an option</button><br><br></div>"
 
-  // Question.prototype.add_option(option_object) {this.question_options.push(option_object)};
-  // Question.prototype.increment_counter{this.question_options_counter = this.option_counter + 1};
+  // };
+  // //what is the position? is this indicating what option it is? maybe this should not be determined by how we pass something into the function. there must be a more viable way to do it.
+
+  // Question.prototype.add_option = function(option_object) {this.question_options.push(option_object)};
+
+  // Question.prototype.increment_counter = function() {this.question_options_counter = this.option_counter + 1};
 
 //--------------------------------------------------------defining function
 
- var question_counter = 2;
-  $("#question_add").on("click", function(event){
-    (event).preventDefault();
 
-    new_question = new Question(question_counter);
-    console.log(new_question.question_html);
 
-    // var option_counter = 1
-    $('#append_question').append(new_question.question_html);
-    question_counter += 1;
-  });
 
-   // $("#question_remove").on("click", function(event){
-   //  (event).preventDefault();
-   //    $("#append_question div:last-child").remove();
-   //    question_counter -= 1;
-   // });
 
-   // $(".option_add_"+option_counter+"").on("click", function(event) {
-   //  (event).preventDefault();
-   //      $("#append_question").append();
-   //  option_counter += 1
-   // });
 
-   // $(".option_remove_"+option_counter+"").on('click', function(event) {
-   //  (event).preventDefault();
 
-   //  option_counter -= 1
-   // });
 
-});
+//  var question_counter = 2;
+//   $("#question_add").on("click", function(event){
+//     (event).preventDefault();
+
+//     new_question = new Question(question_counter);
+//     console.log(new_question.question_html);
+
+//     // var option_counter = 1
+//     $('#append_question').append(new_question.question_html);
+//     question_counter += 1;
+//   });
+
+//    $("#question_remove").on("click", function(event){
+//     (event).preventDefault();
+//       $("#append_question div:last-child").remove();
+//       question_counter -= 1;
+//    });
+
+//    $(".option_add_"+option_counter+"").on("click", function(event) {
+//     (event).preventDefault();
+//         $("#append_question").append();
+//     option_counter += 1
+//    });
+
+//    // $(".option_remove_"+option_counter+"").on('click', function(event) {
+//    //  (event).preventDefault();
+
+//    //  option_counter -= 1
+//    // });
+
+// });
 
 
 //   var question_counter = 2
@@ -126,5 +163,3 @@ $(document).ready(function() {
 //    });
 
 // });
-
-
