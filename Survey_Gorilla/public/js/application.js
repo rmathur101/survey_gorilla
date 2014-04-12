24cc647a6c2a224ac168f0b4fe350ede55c00711
Rohan Mathur <rmathur101@gmail.com>
2014-04-12 13:53:49 -0500

    adding and committing

--- Survey_Gorilla/public/js/application.js
+++ Survey_Gorilla/public/js/application.js
@@ -44,37 +44,87 @@ $(document).ready(function() {
       if (val_password_number(password)){
         $("#check_password").show();
       };
-
-
-
   });
 
-  var question_counter = 2
+
+//so we want to do the following actions using objects instead. how do we accomplish this? we can create a question object. what attributes will a question object have? 
+
+
+  function Question(question_counter){
+    this.question_options = [];
+    this.question_options_counter = 1;
+    this.question_html = "<div class='question_content'>Question " + question_counter +": <input name='survey_question_"+question_counter+"' type='text'><br>Option "+ this.question_options_counter+" <input name='survey_quest_"+question_counter+"_resp"+question_counter+"' type='text'><br><br><button id='option_add_"+this.question_options_counter+"'>Add an option</button><button id='option_remove_"+this.question_options_counter+"'>Remove an option</button><br><br></div>";
+  }; //what we pass into the questoin will determine how we distinguish questions (maybe we should be passing in an index)
+
+  function Option(position) {
+    this.position = position;
+  };
+  //what is the position? is this indicating what option it is? maybe this should not be determined by how we pass something into the function. there must be a more viable way to do it.
+
+  // Question.prototype.add_option(option_object) {this.question_options.push(option_object)};
+  // Question.prototype.increment_counter{this.question_options_counter = this.option_counter + 1};
+
+//--------------------------------------------------------defining function
+
+ var question_counter = 2;
   $("#question_add").on("click", function(event){
     (event).preventDefault();
-    var option_counter = 1
-    $('#append_question').append("<div class='question_content'>Question " + question_counter +": <input name='survey_question_"+question_counter+"' type='text'><br>Option "+option_counter+" <input name='survey_quest_"+question_counter+"_resp"+question_counter+"' type='text'><br><br><button id='option_add_"+option_counter+"'>Add an option</button><button id='option_remove_"+option_counter+"'>Remove an option</button><br><br></div>");
+
+    new_question = new Question(question_counter);
+    console.log(new_question.question_html);
+
+    // var option_counter = 1
+    $('#append_question').append(new_question.question_html);
     question_counter += 1;
   });
 
-   $("#question_remove").on("click", function(event){
-    (event).preventDefault();
-      $("#append_question div:last-child").remove();
-      question_counter -= 1;
-   });
+   // $("#question_remove").on("click", function(event){
+   //  (event).preventDefault();
+   //    $("#append_question div:last-child").remove();
+   //    question_counter -= 1;
+   // });
 
-   $(".option_add_"+option_counter+"").on("click", function(event) {
-    (event).preventDefault();
-        $("#append_question").append();
-    option_counter += 1
-   });
+   // $(".option_add_"+option_counter+"").on("click", function(event) {
+   //  (event).preventDefault();
+   //      $("#append_question").append();
+   //  option_counter += 1
+   // });
 
-   $(".option_remove_"+option_counter+"").on('click', function(event) {
-    (event).preventDefault();
+   // $(".option_remove_"+option_counter+"").on('click', function(event) {
+   //  (event).preventDefault();
 
-    option_counter -= 1
-   });
+   //  option_counter -= 1
+   // });
 
 });
+
+
+//   var question_counter = 2
+//   $("#question_add").on("click", function(event){
+//     (event).preventDefault();
+//     var option_counter = 1
+//     $('#append_question').append("<div class='question_content'>Question " + question_counter +": <input name='survey_question_"+question_counter+"' type='text'><br>Option "+option_counter+" <input name='survey_quest_"+question_counter+"_resp"+question_counter+"' type='text'><br><br><button id='option_add_"+option_counter+"'>Add an option</button><button id='option_remove_"+option_counter+"'>Remove an option</button><br><br></div>");
+//     question_counter += 1;
+//   });
+
+//    $("#question_remove").on("click", function(event){
+//     (event).preventDefault();
+//       $("#append_question div:last-child").remove();
+//       question_counter -= 1;
+//    });
+
+//    $(".option_add_"+option_counter+"").on("click", function(event) {
+//     (event).preventDefault();
+//         $("#append_question").append();
+//     option_counter += 1
+//    });
+
+//    $(".option_remove_"+option_counter+"").on('click', function(event) {
+//     (event).preventDefault();
+
+//     option_counter -= 1
+//    });
+
+// });
 
 
