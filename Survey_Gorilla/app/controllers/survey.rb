024775f91c68713f365d39843ab2b36fa7b33343
harry <apprentice@dbc-chicago-022.local>
2014-04-12 18:44:14 -0500

    Let's do it

--- Survey_Gorilla/app/controllers/survey.rb
+++ Survey_Gorilla/app/controllers/survey.rb
@@ -9,6 +9,22 @@ get '/surveys/take/:id' do
 end
 
 post '/push_survey_data' do
-  Survey.create!()
-  erb :"surveys/show"
+  # assume session[:user_id] gives you the user_id
+  # user_id = session[:user_id]
+  @user = User.find(1)
+  user_id = User.first.id;
+
+  # Round.create!() The only thing being passed in is survey_id and user_id
+  # Response.create!() Pass in Round.id (from last round (created above)) and Choice.id
+  puts "HERE ARE THE PARAMS"
+  p params
+    Round.create!(taker_id: user_id, survey_id: params["survey_id"])
+  params.each do |question_id|
+    p question_id
+    unless question_id[0] == "survey_id"
+      Response.create!(round_id: Round.last.id, choice_id: params[question_id])
+    end
+  end
+
+  erb :"users/dashboard"
 end
