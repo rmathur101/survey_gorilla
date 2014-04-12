1406475fb3e05d4a6b5431a67b691a5f73d831fa
Apprentice <apprentice@dbc-chicago-024.local>
2014-04-12 16:36:50 -0500

    added a logout button

--- Survey_Gorilla/app/controllers/index.rb
+++ Survey_Gorilla/app/controllers/index.rb
@@ -3,23 +3,6 @@ get '/' do
   erb :index
 end
 
-post '/new_user' do
-  @user = User.create(name: params[:name], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
-  session[:user_id] = @user.id
-  erb :"users/dashboard"
-end
-
-post '/user' do
-  @user = User.find_by_email(params[:email])
-
-  if @user &&  @user.authenticate(params[:password])
-    session[:user_id] = @user.id
-    erb :"users/dashboard"
-  else
-    @error = "Wrong information, You fail at life!"
-    erb :index
-  end
-end
 
 # get '/survey_options' do
 #   erb :survey_options
